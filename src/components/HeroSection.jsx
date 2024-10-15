f513fcaf5d5a14f768a07f284ec5bb48e23fd345
Morie Keita <kmorie18c@njala.edu.sl>
2024-10-15 02:06:51 +0000

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,49 @@
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Welcome to Azlan-VR where Innovation meet Excellence
+      </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        "We embodies courage, strength, and integrity empowering bold dreamers
+        to pursue greatness with unwavering loyalty and bravery."
+      </p>
+      <div className="flex justify-center my-10">
+        <a
+          href="#"
+          className="bg-gradient-to-r from-blue-800 to-yellow-300 py-3 px-4 mx-3 rounded-md"
+        >
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-blue-800 shadow-sm shadow-yellow-300 mx-2 my-4"
+        >
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video
+          autoPlay
+          loop
+          muted
+          className="rounded-lg w-1/2 border border-blue-800 shadow-sm shadow-yellow-300 mx-2 my-4"
+        >
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
